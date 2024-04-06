1b2ea4d77944c055e9bcb0432ded09e99c461b78
jnluis <jnluis@ua.pt>
2024-04-06 14:41:46 +0100

    Call External API is almost done

--- HW1/backend/src/main/java/deti/tqs/ua/HW1/service/CurrExchangeService.java
+++ HW1/backend/src/main/java/deti/tqs/ua/HW1/service/CurrExchangeService.java
@@ -1,10 +1,17 @@
 package deti.tqs.ua.HW1.service;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,10 +24,13 @@ public class CurrExchangeService {
     Map<String, Object> cachedRates = new HashMap<>();
     private long lastCaching = 0;
     private int cacheTTL = 3600 * 100;
+    private String apiKey;
 
     //@Autowired
-    public CurrExchangeService () {}
-
+    public CurrExchangeService () {
+        Dotenv dotenv = Dotenv.load();
+        this.apiKey = dotenv.get("EXCHANGE_RATE_API_KEY");
+    }
 
     public CurrExchangeService(int TTL) {
         cacheTTL = TTL;
@@ -33,17 +43,42 @@ public class CurrExchangeService {
         logger.info("Caching exchange rates at " + lastCaching + " for " + cacheTTL + " milliseconds" );
         return true;
     }
-    public double exchange(String from, String to) throws Exception {
 
+    public String RequestAPI(String link) throws Exception {
+        URL url = new URL(link);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
         try {
-            Double rate = Double.parseDouble(cachedRates.get(to).toString());
-            logger.info("Cache hit, returning exchange rate");
-            return rate;
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new Exception("Currency not found");
-        }
+            // Set HTTP method and timeouts
+            con.setRequestMethod("GET");
+            con.setConnectTimeout(15000); // 15 seconds connection timeout
+            con.setReadTimeout(15000); // 15 seconds read timeout
 
+            //HTTP response code
+            int status = con.getResponseCode();
+            if (status < HttpURLConnection.HTTP_BAD_REQUEST) {
+                // Read response from input stream for successful connections
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                    return readResponse(reader);
+                }
+            } else {
+                // Read error message from error stream for failed connections
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(con.getErrorStream()))) {
+                    String response = readResponse(reader);
+                    throw new Exception("HTTP error code: " + status + ". Server response: " + response);
+                }
+            }
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private String readResponse(BufferedReader reader) throws IOException {
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = reader.readLine()) != null) {
+            response.append(inputLine);
+        }
+        return response.toString();
     }
 
     public List<String> listCurrencies() {
@@ -52,5 +87,43 @@ public class CurrExchangeService {
             logger.error("Currencies list not found/null");
         }
         return currencies;
+    }
+
+    public boolean isCacheValid() {
+        return !cachedRates.isEmpty() && lastCaching != 0 && System.currentTimeMillis() < lastCaching + cacheTTL;
+    }
+
+    public double exchange(String from, String to) throws Exception {
+
+        if (isCacheValid()) {
+
+            Double rate = Double.parseDouble(cachedRates.get(to).toString());
+            logger.info("Cache hit, returning exchange rate");
+            return rate;
+
+        } else {
+            logger.info("Cache is not valid, redoing exchange rates request");
+        }
+
+        String api_link = "https://v6.exchangerate-api.com/v6/" + apiKey + "/latest/" + from;
+
+        String content = RequestAPI(api_link);
+
+        JSONObject obj = new JSONObject(content.toString());
+
+        storeExchangeRates(obj.getJSONObject("conversion_rates").toMap());
+
+        currencies = (List<String>) obj.getJSONObject("conversion_rates").keySet();
+
+        double rate;
+
+        try {
+            rate = obj.getJSONObject("conversion_rates").getDouble(to);
+        } catch (Exception e) {
+            throw new Exception("Currency not found");
+        }
+
+        return rate;
+
     }
 }
